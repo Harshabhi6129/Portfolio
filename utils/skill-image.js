dadc18c77d6cf679fef8a227162ea8e8e4791273
HARSHA ABHINAV <140812124+Harshabhi6129@users.noreply.github.com>
2025-04-16 12:35:35 -0400

    Update skill-image.js

--- utils/skill-image.js
+++ utils/skill-image.js
@@ -85,7 +85,7 @@ import linux from "../app/assets/svg/skills/linux.svg";
 import sqlalchemy from "../app/assets/svg/skills/sqlalchemy.svg";
 import fastapi from "../app/assets/svg/skills/fastapi.svg";
 import chromadb from "../app/assets/svg/skills/chromadb.svg";
-import matplotlib from "../app/assets/svg/skills/Matplotlib_icon.svg.png";
+import matplotlib from "../app/assets/svg/skills/Matplotlib_icon.svg";
 export const skillsImage = (skill) => {
 	const skillID = skill.toLowerCase();
 	switch (skillID) {
