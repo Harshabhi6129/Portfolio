db86ba95e4e1b5c4c6819c807474b23654f80b3f
HARSHA ABHINAV <140812124+Harshabhi6129@users.noreply.github.com>
2025-04-16 11:15:06 -0400

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
-	email: "bhavnamallineni@gmail.com",
-	phone: "+91 8008483447",
-	address: "Nellore, Andhra Pradesh, India - 52345 ",
+	email: "harshakusampudi@gmail.com",
+	phone: "+1 7163194521",
+	address: "112 Niagara Falls Blvd, Buffalo, New York - 14214, USA",
 	github: "https://github.com/Harshabhi6129",
 	facebook: "",
 	linkedIn: "https://www.linkedin.com/in/harsha-abhinav/",
